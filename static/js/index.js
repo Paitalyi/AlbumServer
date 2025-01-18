9f61ced4c0024fb885de7a45ef7a3f221d26ed9d
Paitalyi <maopengyang@foxmail.com>
2025-01-18 10:32:32 +0800

    Modify the folding method of nav-drop

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,20 @@ window.addEventListener("pageshow", function(event) {
 	}
 });
 
-document.querySelector('.sidebar-trigger').addEventListener('click', function() {
-	document.querySelector('.nav-drop').classList.toggle('active');
+document.querySelector('.sidebar-trigger').addEventListener('click', function(event) {
+  event.stopPropagation(); // 阻止事件冒泡，以免触发 document 上的点击事件
+  const navDrop = document.querySelector('.nav-drop');
+  navDrop.classList.toggle('active');
 });
+
+// 点击页面其他地方时收起 .nav-drop
+document.addEventListener('click', function(event) {
+  const navDrop = document.querySelector('.nav-drop');
+  const sidebarTrigger = document.querySelector('.sidebar-trigger');
+
+  // 如果点击的不是 sidebar-trigger 和 nav-drop 元素本身，则收起 nav-drop
+  if (!sidebarTrigger.contains(event.target) && !navDrop.contains(event.target)) {
+    navDrop.classList.remove('active');
+  }
+});
+
